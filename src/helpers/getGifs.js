fde3f183b67c64d75f359f2a80c806a62a368a83
Wilson Jauregui M <wjaumen@gmail.com>
2022-03-20 11:01:46 -0400

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=20&api_key=Pxe9BA9ZlDVmE2pvs5x6F9xZ3zmC1ZIX`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
